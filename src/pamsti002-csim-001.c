2c8481f35ca1c77a101b7a17b7b9e493957b9124
olduathell <olivier.durand@logicair.ch>
2015-01-13 12:02:58 +0100

    INIT - First Commit

---
+++ src/pamsti002-csim-001.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : pamsti002-csim-001.c
+ Author      : Olivier Durand
+ Version     : r002-001
+ Copyright   : (c) 2015
+ Description : NFC simulator to interact with python GUI through named pipe
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hi, universe!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
